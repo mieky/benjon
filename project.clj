602107d7cd65d44f73699f452ac244ee84e213d6
Mike Arvela <mike.arvela@futurice.com>
2015-11-14 15:14:18 +0200

    Refresh stub based on compojure template

--- project.clj
+++ project.clj
@@ -1,9 +1,17 @@
 (defproject benjon "0.1.0-SNAPSHOT"
   :description "a backend made of bensonite"
   :url "http://github.com/mieky/benjon"
+  :min-lein-version "2.5.1"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [compojure "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler benjon.core/app}
   :main ^:skip-aot benjon.core
+  :uberjar-name "benjon-standalone.jar"
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring/ring-mock "0.3.0"]]}})
--- src/benjon/core.clj
+++ src/benjon/core.clj
@@ -1,7 +1,17 @@
 (ns benjon.core
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]])
   (:gen-class))
+
+(defroutes app-routes
+  (GET "/" [] "Hello world!")
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Note to self: implement API"))
