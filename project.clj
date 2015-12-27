794bd7e818741511e988274dc3fbe3fb1ecc18b3
Mike Arvela <mike.arvela@futurice.com>
2015-12-27 15:42:11 +0200

    Ditch lein-ring, use httpkit instead

--- project.clj
+++ project.clj
@@ -1,15 +1,14 @@
-(defproject benjon "0.1.0-SNAPSHOT"
+(defproject benjon "0.1.0"
   :description "a backend made of bensonite"
   :url "http://github.com/mieky/benjon"
-  :min-lein-version "2.5.1"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [compojure "1.4.0"]]
-  :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler benjon.core/app}
+                 [compojure "1.4.0"]
+                 [http-kit "2.1.18"]]
   :main ^:skip-aot benjon.core
   :uberjar-name "benjon-standalone.jar"
+  :min-lein-version "2.5.1"
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
--- src/benjon/core.clj
+++ src/benjon/core.clj
@@ -3,15 +3,22 @@
             [compojure.handler :as handler]
             [compojure.route :as route]
             [compojure.core :refer :all]
+            [org.httpkit.server :refer [run-server]]
             [ring.middleware.multipart-params :as mp])
   (:gen-class))
+
+(defn get-server-port []
+  (let [port (System/getenv "PORT")]
+    (if (nil? port)
+      3000
+      (Integer/parseInt port))))
 
 (defn upload-file [file]
   (let [tempfile (get file :tempfile)
         filename (get file :filename)
         targetfile (io/file "resources" "public" filename)]
     (println (str "tempfile: " tempfile " filename: " filename))
-    (io/make-parents targetfile) 
+    (io/make-parents targetfile)
     (io/copy tempfile targetfile)
     "Success!"))
 
@@ -22,10 +29,8 @@
       (upload-file (get params "file"))))
   (route/not-found "Not Found"))
 
-(def app
-  (-> (handler/api app-routes)))
-
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Note to self: implement API"))
+  (let [port (get-server-port)]
+    (println (format "Starting server at localhost:%s..." port))
+    (run-server app-routes {:port port})))
