827574c55ef88e1d0ef7f71b9efed2ebcbd532dd
Mike Arvela <mike.arvela@futurice.com>
2016-01-02 21:50:21 +0200

    Separate initialization sql from that managed by yesql

---
+++ src/sql/initialize.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE benjon;
+CREATE USER benjon WITH PASSWORD 'benjon';
+GRANT ALL PRIVILEGES ON DATABASE benjon TO benjon;
+
+CREATE TABLE IF NOT EXISTS message
+    (id varchar(36) primary key unique, date timestamp default current_timestamp, message text not null);
+
+INSERT INTO message (id, message) VALUES
+    ('ms-1-ca8-a966-4e3e-b821-638f64fd9176', 'well hello!'),
+    ('ms-2-ca8-a966-4e3e-b821-638f64fd9176', 'second one...');
